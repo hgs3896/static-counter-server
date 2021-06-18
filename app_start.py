3637a1d1da6dd8dbdab32eec41ff59c36105d24b
함지성 <hgs3896@naver.com>
2021-06-18 20:34:25 +0900

    메시지 크기에 맞는 이미지를 생성하도록 변경 & 캐싱

--- app_start.py
+++ app_start.py
@@ -1,29 +1,38 @@
-from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+from flask import Flask, Response
 from PIL import Image, ImageDraw, ImageFont
-from datetime import date, datetime
+from datetime import datetime, date, timedelta
+from functools import wraps
 from os import path, environ
-import sys
-import time
 
-from werkzeug.utils import invalidate_cached_property
 app = Flask(__name__)
 app.debug = False
 port = 8080
 
-# Routes
-# @app.route('/', methods=['GET'])
-# def root():
-#     return app.send_static_file('base')
+def docache(hours=24):
+    """ Flask decorator that allow to set Expire and Cache headers. """
+    def fwrap(f):
+        @wraps(f)
+        def wrapped_f(*args, **kwargs):
+            r = f(*args, **kwargs)            
+            next_day = datetime.fromisoformat(datetime.utcnow().strftime("%Y-%m-%d")) + timedelta(hours=hours+9)            
+            r.headers.add('Expires', next_day.strftime("%a, %d %b %Y %H:%M:%S GMT"))
+            r.headers.add('Cache-Control', 'public,max-age=%d' % int(60 * 60 * hours))
+            return r
+        return wrapped_f
+    return fwrap
 
 @app.route('/<int:year>/<int:month>/<int:day>', methods=['GET'])
-def serve_image(year, month, day):
-    invalid_date = not (1990 <= year <= 2021 and 1 <= month <= 12 and 1 <= day <= 31)
+@docache()
+def serve_image(year, month, day) -> Response:
+    validity = 1990 <= year <= 2021 and 1 <= month <= 12 and 1 <= day <= 31
+    
+    dday = datetime.now()
     try:
         dday = date(year, month, day)
-    except:
-        invalid_date = True
+    except ValueError as e:
+        validity = False
     
-    if invalid_date:
+    if not validity:
         return app.send_static_file('base.jpg')
     
     filename = f'{year:04d}-{month:02d}-{day:02d}.jpg'
@@ -36,19 +45,23 @@ def serve_image(year, month, day):
         if cur_time.day == last_modified_time.day:
             return app.send_static_file(filename)
 
-    target_img = Image.new("RGB", (270, 40), color = (255, 255, 255))    
-    draw = ImageDraw.Draw(target_img)
     msg = ''
-    
     if days_left > 0:
         msg = f'전역까지 {days_left}일 남았습니다.'
     elif days_left <= 0:
         msg = f'{year:04d}/{month:02d}/{day:02d}부로 전역했습니다!'
-    
-    fnt = ImageFont.truetype("fonts/강한육군 Medium.ttf", 20, encoding="UTF-8")
-    draw.text((10, 10), msg, font=fnt, fill=(0, 0, 0), align='center')
-    target_img.save('static/' + filename)
+
+    make_static_image(filename, msg)    
     return app.send_static_file(filename)
+
+def make_static_image(filename, msg):    
+    fnt = ImageFont.truetype("fonts/강한육군 Medium.ttf", 20, encoding="UTF-8")
+    w, h = fnt.getsize(msg)
+
+    target_img = Image.new("RGB", (w + 20, h + 20), color = (255, 255, 255))
+    draw = ImageDraw.Draw(target_img)
+    img_msg = draw.text((10, 10), msg, font=fnt, fill=(0, 0, 0), align='center')    
+    target_img.save('static/' + filename)
 
 if __name__ == "__main__":
     try:
